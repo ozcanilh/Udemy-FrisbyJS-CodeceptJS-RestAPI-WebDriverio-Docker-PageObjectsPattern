030860cb2ac7ea9ea62750521e8b0d6cfce6b11b
ozcanilh <33623618+ozcanilh@users.noreply.github.com>
2018-02-02 19:21:34 +0300

    Update frisby.sh

--- frisby.sh
+++ frisby.sh
@@ -1,5 +1,9 @@
-npm install frisby@2.0.5 
+npm install jasmine
 npm install jasmine-node 
 npm install jsonschema 
 npm install content-type
 npm install jest
+npm install jest-cli
+npm install request
+npm install frisby@2.0.5 
+npm install body-parser
