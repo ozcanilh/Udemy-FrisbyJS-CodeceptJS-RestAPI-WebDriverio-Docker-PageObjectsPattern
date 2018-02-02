512133c78eebdfb575b2078ccd7731664bedd444
ozcanilh <33623618+ozcanilh@users.noreply.github.com>
2018-02-02 19:21:07 +0300

    Update codecep.sh

--- codecep.sh
+++ codecep.sh
@@ -1,7 +1,7 @@
+npm install npm-run-all
 npm install codeceptjs@1.1.1 
 npm install webdriverio 
 npm install selenium-webdriver 
 npm install selenium-standalone@latest 
 selenium-standalone install
-selenium-standalone start
 npm install co
